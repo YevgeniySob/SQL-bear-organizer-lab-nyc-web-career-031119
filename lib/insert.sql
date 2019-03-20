3b703baaa8e21467426eb3641eb57103f65eca02
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-03-20 17:17:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'male', 'blue', 'supernice', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'male', 'blue', 'supernice', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'male', 'blue', 'supernice', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'male', 'blue', 'supernice', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'male', 'blue', 'supernice', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'male', 'blue', 'supernice', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergent', 2, 'male', 'blue', 'supernice', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'male', 'blue', 'supernice', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'male', 'blue', 'supernice', 1);
